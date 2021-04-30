c211026299a63311b78e21d07290d96f6ca787f0
JK <38530893+JaraKucera@users.noreply.github.com>
2021-04-30 16:22:55 +0100

    travis testing2

---
+++ setup.py
@@ -0,0 +1,12 @@
+  
+from setuptools import setup
+
+setup(name='algorithm',
+      version='0.1',
+      description='x',
+      url='x',
+      author='x',
+      author_email='x',
+      license='Apache-2.0',
+      packages=find_packages(),
+      zip_safe=False)
